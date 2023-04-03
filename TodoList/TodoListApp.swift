752754d85eb2a0635a00236a1cef0f72cb7ef732
Komal Gilani <komalsyeda29@gmail.com>
2023-04-03 15:12:46 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Komal Gilani on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
